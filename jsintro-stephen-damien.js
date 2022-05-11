8d8b06a2596b867da0454bc0761203dceceeb21a
LEARN Academy <learnacademy@LEARNs-MacBook-Air-3.local>
2022-05-11 14:06:59 -0700

    completed the first 10 questions

---
+++ jsintro-stephen-damien.js
@@ -0,0 +1,20 @@
+// 1. Write the code that will log the outcome of 34 added to 71.
+console.log(34+71)
+// 2. Write the code that will log the outcome of 67 subtracted from 123.
+console.log(123-67)
+// 3. Write the code that will log the outcome of 56 multiplied by 23.
+console.log(56*23)
+// 4. Write the code that will log the outcome of 45 divided by 5.
+console.log(45/5)
+// 5. Write the code that will log the outcome of 5 to the power of 7.
+console.log(5**7)
+// 6. Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33%6)
+// 7. Write the code that will log the length of a string containing your name.
+console.log("stephen".length)
+// 8. Write the code that will log whether your string includes the letter "e"?
+console.log("stephen".includes("e"))
+// 9. Write the code that will log the character at the first index of the string.
+console.log("stephen"[0])
+// 10. Write the code that will log the string in all uppercase letters.
+console.log("stephen".toUpperCase())
