e80f1fe01c986068fc131c425cc69be17315fa47
Damien Richcreek <damien@richcreek.dev>
2022-05-11 14:17:51 -0700

    completed 11-20 and included format for 21-27

--- jsintro-stephen-damien.js
+++ jsintro-stephen-damien.js
@@ -18,3 +18,51 @@ console.log("stephen".includes("e"))
 console.log("stephen"[0])
 // 10. Write the code that will log the string in all uppercase letters.
 console.log("stephen".toUpperCase())
+
+
+// ||== Answers Should Be True or False ==|| //
+console.log("\n ||=== Answers Should Be true or false ====|| \n")
+
+// 11. Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34/3 > 67/2)
+// 12. Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
+// 13. Does 5 strictly equal "5"?
+console.log(5 === "5")
+// 14. Does !3 strictly equal 3?
+console.log(!3 === 3)
+// 15. Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
+// 16. Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10)
+// 17. Does "LEARN" contain the subset "RN"?
+console.log("LEARN".includes("RN"))
+// 18. Does "LEARN" contain the subset "rn"?
+console.log("LEARN".includes("rn"))
+// 19. Does "LEARN"[0] strictly equal "l"?
+console.log("LEARN"[0] === "l")
+// 20. Modify the code from the previous question to return true.
+console.log("LEARN"[0] === "L")
+
+// ||== Consider the Variables ==|| //
+console.log("\n||=== Consider the Variables                                   ====||")
+console.log("||=== var theQuestion = \"life, the universe, and everything\"   ====||")
+console.log("||=== var theAnswer = 42                                       ====||\n")
+
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+
+// 21. Write the code that will log theAnswer divided by 2.
+console.log()
+// 22. Write the code that will log the length of theQuestion.
+console.log()
+// 23. Write the code that will log the index of the character "f" in the theQuestion.
+console.log()
+// 24. Write the code that will log the concatenation of the two variables.
+console.log()
+// 25. Write the code that will log "the universe".
+console.log()
+// 26. Write the code that will log the character "l" from theQuestion.
+console.log()
+// 27. Write the code that will log whether theQuestion.length is greater than theAnswer.
+console.log()
