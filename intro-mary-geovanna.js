84d0e2db189ec6162236a4c788df32b05eedb4c8
mdelc <chemandelcarpio@yahoo.com>
2022-05-11 15:56:04 -0700

    Mary's updated contribution to qs

--- intro-mary-geovanna.js
+++ intro-mary-geovanna.js
@@ -35,12 +35,30 @@ console.log("mary".toUpperCase())
 // Write the code that will log true or false for the following:
 //
 // Is 34 divided by 3 greater than 67 divided by 2?
+var num1 = 43
+var num2 = 3
+var num3 = 67
+var num4 = 2
+
+console.log(num1 / num2 > num3 / num4)
+
 // Does 5 evaluate to the same as "5"?
-// Does 5 strictly equal "5"?
+
+console.log(5 === "5")
+// Does 5 strictly equal "5
+
 // Does !3 strictly equal 3?
+
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
+
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+
+console.log("LEARN".length === 5 || "Student".length === 10)
 // Does "LEARN" contain the subset "RN"?
+console.log("LEARN".includes("RN"))
+
 // Does "LEARN" contain the subset "rn"?
 // Does "LEARN"[0] strictly equal "l"?
 // Modify the code from the previous question to return true.
