5f7f00b3a558f333368e8fa5514c6da2c1807d37
michael-a-tobias <michael.a.tobias@outlook.com>
2022-05-11 14:48:12 -0700

    initial upload

---
+++ jschallenge-michael-frank.js
@@ -0,0 +1,108 @@
+// JAVASCRIPT CHALLENGE
+// Write the code that will log the outcome of 34 added to 71.
+var num1 = 34
+var num2 = 71
+console.log(num1 + num2)
+// Write the code that will log the outcome of 67 subtracted from 123.
+var num3 = 67
+var num4 = 123
+console.log(num4 - num3)
+// Write the code that will log the outcome of 56 multiplied by 23.
+var num5 = 56
+var num6 = 23
+console.log(num5 * num6)
+// Write the code that will log the outcome of 45 divided by 5.
+var num7 = 45
+var num8 = 5
+console.log(num7 / num8)
+// Write the code that will log the outcome of 5 to the power of 7.
+var num9 = 5
+var num10 = 7
+console.log(num9 ** num10)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+var a1 = 33
+var a2 = 6
+console.log(a1 % a2)
+// Write the code that will log the length of a string containing your name.
+var name = "michael"
+console.log(name.length)
+// Write the code that will log whether your string includes the letter "e"?
+console.log(name.includes("e"))
+// Write the code that will log the character at the first index of the string.
+console.log(name[0])
+// Write the code that will log the string in all uppercase letters.
+console.log(name.toUpperCase())
+// Write the code that will log true or false for the following:
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log(
+    (34 / 3) > (67 / 2)
+)
+// Does 5 evaluate to the same as "5"?
+console.log(
+    5 == "5"
+)
+// Does 5 strictly equal "5"?
+console.log(
+    5 === "5"
+)
+// Does !3 strictly equal 3?
+console.log(
+    !3 === 3
+)
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log(
+    "LEARN".length === 5 && "Student".length === 7
+)
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log(
+    "LEARN".length === 5 || "Student".length === 10
+)
+// Does "LEARN" contain the subset "RN"?
+ console.log(
+     "LEARN".includes("RN")
+ )
+// Does "LEARN" contain the subset "rn"?
+console.log(
+    "LEARN".includes("rn")
+)
+// Does "LEARN"[0] strictly equal "l"?
+console.log(
+    "LEARN"[0] === "l"
+    )
+// Modify the code from the previous question to return true.
+console.log(
+    "lEARN"[0] === "l"
+    )
+// Consider the variables:
+
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+
+// Write the code that will log theAnswer divided by 2.
+console.log(
+ theAnswer / 2   
+)
+// Write the code that will log the length of theQuestion.
+console.log(
+    theQuestion.length
+    )
+// Write the code that will log the index of the character "f" in the theQuestion.
+console.log(
+    theQuestion.indexOf("f")
+    )
+// Write the code that will log the concatenation of the two variables.
+console.log(
+    theQuestion + theAnswer
+    )
+// Write the code that will log "the universe".
+console.log(
+    "the universe"
+    )
+// Write the code that will log the character "l" from theQuestion.
+console.log(
+    theQuestion.indexOf("l")
+    )
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
+console.log(
+    theQuestion.length > theAnswer.length
+    )
