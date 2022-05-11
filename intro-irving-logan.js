fb4f55eb48f5a76f1c5cc1855c6c576001b90e0f
Renzlawr <RSLogan23@gmail.com>
2022-05-11 15:37:56 -0600

    More questions answered

--- intro-irving-logan.js
+++ intro-irving-logan.js
@@ -10,18 +10,30 @@ console.log(56 * 23)
 // Write the code that will log the outcome of 45 divided by 5.
 console.log(45 / 5)
 // Write the code that will log the outcome of 5 to the power of 7.
+console.log(5 ** 7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6)
 // Write the code that will log the length of a string containing your name.
+console.log("Logan".length)
 // Write the code that will log whether your string includes the letter "e"?
+console.log("Logan".includes("e"))
 // Write the code that will log the character at the first index of the string.
+console.log("Logan"[0])
 // Write the code that will log the string in all uppercase letters.
+console.log("Logan".toUpperCase())
 // Write the code that will log true or false for the following:
 //
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
 // Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
 // Does 5 strictly equal "5"?
+console.log(5 === "5")
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+var learn = "LEARN"
+console.log()
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
 // Does "LEARN" contain the subset "RN"?
 // Does "LEARN" contain the subset "rn"?
