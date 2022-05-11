ff6006d7f41c770133651174e9a1b51984b36bea
Geovannapg12 <geo.p2334@gmail.com>
2022-05-11 14:34:33 -0700

    Challenge questions

---
+++ intro-mary-geovanna.js
@@ -0,0 +1,57 @@
+// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
+//
+// Write the code that will log the outcome of 34 added to 71.
+var num1=34
+var num2=71
+console.log(num1 + num2)
+// Write the code that will log the outcome of 67 subtracted from 123.
+var num3=123
+var num4=67
+console.log(num3 - num4)
+// Write the code that will log the outcome of 56 multiplied by 23.
+var num5=56
+var num6=23
+console.log(num5 * num6)
+// Write the code that will log the outcome of 45 divided by 5.
+var num7=45
+var num8=5
+console.log(num7 / num8)
+// Write the code that will log the outcome of 5 to the power of 7.
+var num9=5
+var num10=7
+console.log(num9 ** num10)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+var num01=33
+var num02=6
+console.log(num01 % num02)
+// Write the code that will log the length of a string containing your name.
+console.log("geovanna".length)
+// Write the code that will log whether your string includes the letter "e"?
+console.log("geovanna".includes("e"))
+// Write the code that will log the character at the first index of the string.
+console.log("Mary"[0])
+// Write the code that will log the string in all uppercase letters.
+console.log("mary".toUpperCase())
+// Write the code that will log true or false for the following:
+//
+// Is 34 divided by 3 greater than 67 divided by 2?
+// Does 5 evaluate to the same as "5"?
+// Does 5 strictly equal "5"?
+// Does !3 strictly equal 3?
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
+// Consider the variables:
+//
+// var theQuestion = "life, the universe, and everything"
+// var theAnswer = 42
+// Write the code that will log theAnswer divided by 2.
+// Write the code that will log the length of theQuestion.
+// Write the code that will log the index of the character "f" in the theQuestion.
+// Write the code that will log the concatenation of the two variables.
+// Write the code that will log "the universe".
+// Write the code that will log the character "l" from theQuestion.
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
