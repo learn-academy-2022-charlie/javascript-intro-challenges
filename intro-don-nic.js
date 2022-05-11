efac52624e9fc31121e47c3cb3f7e5183ac40d2c
Nic Last <niclast7611@gmail.com>
2022-05-11 14:35:22 -0700

    Questions finished up to considering the variables

--- intro-don-nic.js
+++ intro-don-nic.js
@@ -27,22 +27,41 @@ numTwo = 7;
 console.log(5 ** 7)
 
 // Write the code that will log the whole number remainder of 33 divided by 6.
+numOne = 33;
+numTwo = 6;
+console.log(numOne % numTwo)
 // Write the code that will log the length of a string containing your name.
+var myName = 'nic'
+console.log(myName.length);
 // Write the code that will log whether your string includes the letter "e"?
+console.log(myName.includes('e'))
 // Write the code that will log the character at the first index of the string.
+console.log(myName[0])
 // Write the code that will log the string in all uppercase letters.
+console.log(myName.toUpperCase())
+
 // Write the code that will log true or false for the following:
 
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
 // Does 5 evaluate to the same as "5"?
+console.log(5 == '5')
 // Does 5 strictly equal "5"?
+console.log(5 === '5')
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log('LEARN'.length === 5 && 'Student'.length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log('LEARN'.length === 5 || 'Student'.length === 10)
 // Does "LEARN" contain the subset "RN"?
+console.log('LEARN'.includes('RN'))
 // Does "LEARN" contain the subset "rn"?
+console.log('LEARN'.includes('rn'))
 // Does "LEARN"[0] strictly equal "l"?
+console.log('LEARN'[0] === 'l')
 // Modify the code from the previous question to return true.
+console.log('LEARN'[0] === 'L')
 // Consider the variables:
 
 // var theQuestion = "life, the universe, and everything"
