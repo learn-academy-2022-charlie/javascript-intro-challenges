04f740824344d50bf1e11fe02373035175c120cc
OnyxFire1 <alexneil179@gmail.com>
2022-05-12 16:15:48 -0700

    Solved first question

--- loops-denise-sean.js
+++ loops-denise-sean.js
@@ -1,4 +1,7 @@
 // Create a for loop that logs each number from 1 - 20.
+for(let i = 0; i<20; i++) {
+    console.log(i)
+}
 // Create a for loop that logs every other number from 1 - 20.
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
