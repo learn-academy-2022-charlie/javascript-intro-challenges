d6a6f618e71b286d401108dff75edbebb3e56f59
Don Yoon <dongho.yoon@gmail.com>
2022-05-12 11:47:17 -0700

    finished third and fourth problems set

--- arrays-don-nicholas.js
+++ arrays-don-nicholas.js
@@ -4,6 +4,8 @@
 // Consider the variable:
 
 var groceryList = ["chips", "dip", "cookies"]
+// groceryList will only be declared once, we as a group understand that the variable is being modified as throughout the exercise
+
 // Write the code that will add "soda" to the end of the original array.
 groceryList.push("soda")
 console.log(groceryList)
@@ -28,6 +30,7 @@ console.log(groceryListTwo)
 // Consider the variable:
 
 var numbers = [2, 4, 6, 8, 10]
+// numbers will only be declared once, we as a group understand that the variable is being modified as throughout the exercise
 
 // Write the code that will add the number 0 to the beginning of the array.
 numbers.unshift(0)
@@ -49,31 +52,56 @@ console.log(numbers)
 
 // Consider the variable:
 
-// var numSet = [2, 13, 6, 8, 4, 2]
+var numSet = [2, 13, 6, 8, 4, 2]
+// numSet will only be declared once, we as a group understand that the variable is being modified as throughout the exercise
+
+
 // Write the code that finds the index of the first appearance of the number 2.
+console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
+console.log(numSet.lastIndexOf(2))
 
 // Write the code that returns the number at the third index.
+console.log(numSet[3])
 
 // Consider the variable:
 
-// var characters = ["y", "a", "r", "r", "a"]
+var characters = ["y", "a", "r", "r", "a"]
+// characters will only be declared once, we as a group understand that the variable is being modified as throughout the exercise
+
 // Write the code that brings all the letters in the characters array together into a string.
+//console.log(characters) this is the array
+console.log(characters.join(""))
 
 // Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
+var charsReversed = characters.reverse()
+console.log(charsReversed)
 
 // Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
+console.log(charsReversed.join("*"))
 
 // Write the code that brings all the letters in the charsReversed array together into a string without separators.
+console.log(charsReversed.join(""))
 
 // Create two arrays consisting of three first names of your cohort members in each.
+var learnStudentsA = ["Frank", "Valorie", "Denise"]
+var learnStudentsB = ["Michael", "CJ", "Sean"]
 
 // Write the code that sorts the names in alphabetical order.
+console.log(learnStudentsA.sort())
+console.log(learnStudentsB.sort())
 
 // Write the code that sorts the names in reverse alphabetical order.
+//Since in the previous step we have sorted in alphabetical order, we just need to reverse
+//console.log(learnStudentsA.reverse())
+//console.log(learnStudentsB.reverse())
+console.log(learnStudentsA.sort((a, b) => -(a>b)||+(a<b)))
+console.log(learnStudentsB.sort((a, b) => -(a>b)||+(a<b)))
 
 // Write the code that sorts all the names in alphabetical order in a single array.
+var learnStudentsAll = learnStudentsA.concat(learnStudentsB)
+console.log(learnStudentsAll.sort())
 
 // Consider the variables:
 
