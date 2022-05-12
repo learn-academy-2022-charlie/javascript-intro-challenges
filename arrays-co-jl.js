9f10cd4eedb38239976599b7ee3937c71e4c6069
LoneWanderer101 <sgtlinares@gmail.com>
2022-05-12 11:30:15 -0700

    updated arrays challenge

--- arrays-co-jl.js
+++ arrays-co-jl.js
@@ -7,25 +7,43 @@
 
 // var groceryList = ["chips", "dip", "cookies"]
 // Write the code that will add "soda" to the end of the original array.
-var groceryList = ["chips", "dip", "cookies"]
-groceryList.push("soda")
-console.log(groceryList)
+
+// var groceryList = ["chips", "dip", "cookies"]
+
+//      groceryList.push("soda")
+//      console.log(groceryList)
 
 // Write the code that will add "granola" to the end of array without altering the original array.
-console.log(groceryList.concat("granola"))
+// console.log(groceryList.concat("granola"))
 
 // Write the code that will return a subset of the array containing only "chips" and "dip".
+// console.log(groceryList.slice(0,2))
 
 // Write the code that will add "beans" to the "chips" and "dip" array.
 
-// Consider the variable:
+        var groceryList = ["chips", "dip", "cookies"]
+        groceryList[2] = "beans"
+        console.log(groceryList)
 
+// Consider the variable:
 // var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
 
+        var numbers = [2, 4, 6, 8, 10]
+        // numbers.unshift(0)
+        // console.log(numbers)
+
 // Write the code that will add the number 12 to the end of the array.
 
+        // console.log(numbers.concat(12))
+        // // or
+        // numbers.push(12)
+        // console.log(numbers)
+
 // Write the code that will remove the first number from the array.
+
+        numbers.shift()
+        console.log(numbers)
 
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
 
