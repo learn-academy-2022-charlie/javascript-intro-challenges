933c17a5900e55ba5cd403ebc88ea5dd19b3c739
IrvingCyberGarcia <irving_garcia17@hotmail.com>
2022-05-12 10:58:42 -0700

    irving contribution

--- arrays-irving-mary.js
+++ arrays-irving-mary.js
@@ -12,3 +12,16 @@ console.log(groceryList.slice(2))
 // Write the code that will add "beans" to the "chips" and "dip" array.
 console.log(groceryList.pop())
 console.log(groceryList.push("beans"))
+
+
+// Consider the variable:
+//
+ var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+console.log(numbers.unshift(0))
+// Write the code that will add the number 12 to the end of the array.
+console.log(numbers.push(12))
+// Write the code that will remove the first number from the array.
+console.log(numbers.shift())
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+console.log(numbers.shift())
