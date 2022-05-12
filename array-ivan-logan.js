360715f987f2f56e5440af5ff1d217f28f70ef2a
Renzlawr <RSLogan23@gmail.com>
2022-05-12 12:04:50 -0600

    questions 5-8 answered

--- array-ivan-logan.js
+++ array-ivan-logan.js
@@ -19,3 +19,25 @@ var groceryList = ["chips", "dip", "cookies"]
 
 	groceryList.push("beans")
 		console.log(groceryList)
+
+
+
+var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+numbers.unshift(0)
+console.log(numbers)
+
+
+// Write the code that will add the number 12 to the end of the array.
+numbers.push(12)
+console.log(numbers)
+// Write the code that will remove the first number from the array.
+numbers.shift(0)
+console.log(numbers)
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+
+var zero = [0]
+
+numbers.concat(zero)
+console.log(numbers.concat(zero).sort((a, b) => a - b))
+
