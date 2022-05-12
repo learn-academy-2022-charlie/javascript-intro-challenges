b3a27036cb2bf6a7d77610db60c5b71e9589188b
ios10 <trikshotzz05@gmail.com>
2022-05-12 10:35:14 -0700

    array first 4

---
+++ array-ivan-logan.js
@@ -0,0 +1,21 @@
+var groceryList = ["chips", "dip", "cookies"]
+
+// Write the code that will add "soda" to the end of the original array.
+
+groceryList.push("soda")
+console.log(groceryList)
+
+// Write the code that will add "granola" to the end of array without altering the original array.
+	var granola = ["granola"] 
+	console.log(groceryList.concat(granola))
+
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+var groceryList = ["chips", "dip", "cookies"]
+
+		groceryList.pop()
+			console.log(groceryList)
+
+// Write the code that will add "beans" to the "chips" and "dip" array.
+
+	groceryList.push("beans")
+		console.log(groceryList)
