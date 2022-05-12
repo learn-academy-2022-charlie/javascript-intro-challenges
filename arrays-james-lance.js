2c5f716db77e4330f69c0c0afde1871bfe87222a
lancewrites <lancewrites77@gmail.com>
2022-05-12 11:32:12 -0700

    lances additions three to eight

--- arrays-james-lance.js
+++ arrays-james-lance.js
@@ -1,27 +1,52 @@
 var groceryList = ["chips", "dip", "cookies"]
 // Write the code that will add "soda" to the end of the original array.
 groceryList.push("soda")
-console.log(groceryList)
+// console.log(groceryList)
 
 
 // Write the code that will add "granola" to the end of array without altering the original array.
 var groceryList2 = ["granola"]
 var comboList = groceryList.concat(groceryList2)
-console.log(comboList)
+// console.log(comboList)
+
 // Write the code that will return a subset of the array containing only "chips" and "dip".
+var groceryList3 = (groceryList.slice(0,2))
+// console.log(groceryList3)
 
 // Write the code that will add "beans" to the "chips" and "dip" array.
+groceryList3.push("beans")
+// console.log(groceryList3)
+
 
 // Consider the variable:
 
-// var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
+
 // Write the code that will add the number 0 to the beginning of the array.
+numbers.reverse()
+numbers.push(0)
+numbers.reverse()
+// console.log(numbers)
+
 
 // Write the code that will add the number 12 to the end of the array.
-
+numbers.push(12)
+// console.log(numbers)
 // Write the code that will remove the first number from the array.
+numbers.reverse()
+// console.log(numbers)
+numbers.pop()
+// console.log(numbers)
+numbers.reverse()
+// console.log(numbers)
 
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var numbers2 = [0]
+numbers.reverse()
+var comboNumbers = numbers.concat(numbers2)
+console.log(comboNumbers)
+comboNumbers.reverse()
+console.log(comboNumbers)
 
 // Consider the variable:
 
