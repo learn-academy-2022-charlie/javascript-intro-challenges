4574bd8c9525c967c463a3cedab2fc669c2fcffa
mdelc <chemandelcarpio@yahoo.com>
2022-05-12 10:31:29 -0700

    Mary's contribution

---
+++ arrays-irving-mary.js
@@ -0,0 +1,14 @@
+// // Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
+//
+// Consider the variable:
+//
+var groceryList = ["chips", "dip", "cookies"]
+// Write the code that will add "soda" to the end of the original array.
+groceryList.push("soda")
+// Write the code that will add "granola" to the end of array without altering the original array.
+console.log(groceryList.concat("granola"))
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+console.log(groceryList.slice(2))
+// Write the code that will add "beans" to the "chips" and "dip" array.
+console.log(groceryList.pop())
+console.log(groceryList.push("beans"))
