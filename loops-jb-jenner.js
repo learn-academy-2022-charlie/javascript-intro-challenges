58c4d7d57462f9857e7ecafcc309f231c48dc516
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-05-12 15:46:27 -0700

    challenge answer

---
+++ loops-jb-jenner.js
@@ -0,0 +1,37 @@
+// Create a for loop that logs each number from 1 - 20.
+
+for(let i = 1; i < 21; i++){ 
+
+    console.log( i )
+}
+
+
+// Create a for loop that logs every other number from 1 - 20.
+
+for(let i = 1; i < 21; i = i + 2){
+    console.log(i)
+}
+
+for(let i = 1; i < 21; i++) {
+    if(i % 2 ===0 ){
+        continue
+    } else {
+        console.log(i)
+        
+    }
+}
+// Create a for loop that logs the result of each number from 1 - 20 tripled.
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+// Consider this variable:
+
+// const nums = [3, 57, -9, 20, 67]
+// Create the code that will log the largest number from the array.
+// Create the code that will log the smallest number from the array.
+// Create the code that will log the remainder of each number when divided by 2.
+// Expected output: 1, 1, -1, 0, 1
+// Consider this variable:
+
+// const myString = "learn student"
+// Create the code that will log the number of times the letter "e" occurs in the string.
+// Create the code that will log every other character in the string.
