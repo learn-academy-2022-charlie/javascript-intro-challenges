6748103546d74d04edfea44011378741b6395775
NicholasLoiacono <NTLoiacono4@gmail.com>
2022-05-12 11:10:34 -0700

    Second problem set complete

--- array-don-nicholas.js
+++ array-don-nicholas.js
@@ -23,16 +23,29 @@ var groceryListTwo = groceryList.slice(0,2)
 groceryListTwo.unshift("beans")
 console.log(groceryListTwo)
 
+//__________________________________________________________
+
 // Consider the variable:
 
-// var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
+
 // Write the code that will add the number 0 to the beginning of the array.
+numbers.unshift(0)
+console.log(numbers)
 
 // Write the code that will add the number 12 to the end of the array.
+numbers.push(12)
+console.log(numbers)
 
 // Write the code that will remove the first number from the array.
+numbers.shift()
+console.log(numbers)
 
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var numbersAdd = [0]
+var numbersNew = numbersAdd.concat(numbers)
+console.log(numbersNew)
+console.log(numbers)
 
 // Consider the variable:
 
