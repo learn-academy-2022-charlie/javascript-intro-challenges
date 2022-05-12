19e5d32cdd09e6dfb729e9bab051076767797409
LEARN Academy <learnacademy@LEARNs-MacBook-Air-3.local>
2022-05-12 11:09:41 -0700

    completed 5-8

--- array-dk-so.js
+++ array-dk-so.js
@@ -5,11 +5,11 @@
 
 var groceryList = ["chips", "dip", "cookies"]
 // Write the code that will add "soda" to the end of the original array.
-// groceryList.push("soda")
-// console.log(groceryList)
+groceryList.push("soda")
+console.log(groceryList)
 // Write the code that will add "granola" to the end of array without altering the original array.
-// var granola = ["granola"]
-// console.log(groceryList.concat(granola));
+var granola = ["granola"]
+console.log(groceryList.concat(granola));
 // Write the code that will return a subset of the array containing only "chips" and "dip".
 console.log(groceryList.slice(0, 2));
 // Write the code that will add "beans" to the "chips" and "dip" array.
@@ -17,15 +17,19 @@ groceryList.unshift("beans")
 console.log(groceryList);
 // Consider the variable:
 
-// var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
-
+numbers.unshift(0)
+console.log(numbers);
 // Write the code that will add the number 12 to the end of the array.
-
+numbers.push(12)
+console.log(numbers);
 // Write the code that will remove the first number from the array.
-
+numbers.shift()
+console.log(numbers);
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
-
+var number0 = [0]
+console.log(number0.concat(numbers)); // 0 + 2 , 4 , 6, 8
 // Consider the variable:
 
 // var numSet = [2, 13, 6, 8, 4, 2]
