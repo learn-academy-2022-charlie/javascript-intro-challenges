7c876504fa4820d2ffc37831bbc0f4328b0e05e9
LoneWanderer101 <sgtlinares@gmail.com>
2022-05-16 13:50:11 -0700

    worked another problem

--- functionloopsarrays-Jorge-Denise-Collin.js
+++ functionloopsarrays-Jorge-Denise-Collin.js
@@ -10,22 +10,37 @@
 // }
 // console.log(mult3(testArr1));
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
- var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+//  var testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
 // // --> [-7, 3, 5, 13]
 
-const onlyOdd = (array) => {
-    let newArr = []
-    for (let i=0; i < array.length; i++){
-        if(array[i] % 2 !== 0){
-            newArr.push(array[i])
-        }
-    } 
-    return newArr
-}
-console.log(onlyOdd(testArr2))
+// const onlyOdd = (array) => {
+//     let newArr = []
+//     for (let i=0; i < array.length; i++){
+//         if(array[i] % 2 !== 0){
+//             newArr.push(array[i])
+//         }
+//     } 
+//     return newArr
+// }
+// console.log(onlyOdd(testArr2))
+
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
 // var comboArr = [7, "n", true, "i", "c", 10, "e", -388, "w", 3, "o", 0, "r", false, "k"]
 // // --> "nicework"
+
+        var comboArr = [7, "n", true, "i", "c", 10, "e", -388, "w", 3, "o", 0, "r", false, "k"]
+        const letterOnly = (array) => {
+            let onlyString = []
+            for (let i=0; i<array.length; i++) {
+            if (typeof array[i] === "string") {
+                onlyString.push(array[i])
+            }
+
+            }    
+                return onlyString.join("")
+        }
+        console.log(letterOnly(comboArr))
+
 // Create a function that takes in an array of numbers and returns the sum.
 // var addThese1 = [1, 2, 3, 4]
 // // --> 10
